a8590c7cbab7e68a9f4984d5ef92bd612bd63ad0
Gokul NC <gokul@multicorewareinc.com>
2018-08-14 10:37:21 +0530

    Add struct definitions and stubs for functions

---
+++ bigInteger.c
@@ -0,0 +1,33 @@
+#include <bigInteger.h>
+#include <string.h>
+
+#define MAX_INT_LENGTH 100
+
+struct BigInt {
+  int sign, numDigits;
+  char number[MAX_INT_LENGTH];
+};
+
+int parseToBigInt(char *string, BigInteger *destination) {
+  return -1;
+}
+
+BigInteger addBigInt(BigInteger *a, BigInteger *b) {
+  BigInteger result;
+  return result;
+}
+
+BigInteger subtractBigInt(BigInteger *a, BigInteger *b) {
+  BigInteger result;
+  return result;
+}
+
+BigInteger multiplyBigInt(BigInteger *a, BigInteger *b) {
+  BigInteger result;
+  return result;
+}
+
+BigInteger divideBigInt(BigInteger *a, BigInteger *b) {
+  BigInteger result;
+  return result;
+}
